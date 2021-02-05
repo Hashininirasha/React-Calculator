79c3831679698199c4f28075843c25b5f4e2b3e8
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-02-05 22:13:40 +0530

    created calculator.jsx

---
+++ src/components/calculator.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class calculator extends Component {
+    render(){
+        return(
+            <div>
+                Cal
+            </div>
+        );
+
+    }
+}
