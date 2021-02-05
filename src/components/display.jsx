613871d7e5491946a345f00109fc1526eeed1add
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-02-05 22:27:15 +0530

    created display.jsx

---
+++ src/components/display.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+class display extends Component{
+    render(){
+        return(
+            <div>
+                Display
+            </div>
+        );
+    };
+}
+
+export default display;
